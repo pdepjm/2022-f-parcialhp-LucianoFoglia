0f26bc698f2f229913fcf339b717a919ad440ca1
LucianoFoglia <lucianofoglia.lf@gmail.com>
2022-06-09 11:55:11 -0300

    Terminado

--- src/Library.hs
+++ src/Library.hs
@@ -34,15 +34,15 @@ type Hechizo = Postre -> Postre
 modificarTemp :: (Number -> Number) -> Postre -> Postre
 modificarTemp modificar postre = postre{temperatura = modificar (temperatura postre)}
 
-modificarpeso :: (Number -> Number) -> Postre -> Postre
-modificarpeso modificar postre = postre{peso = modificar (peso postre)}
+modificarPeso :: (Number -> Number) -> Postre -> Postre
+modificarPeso modificar postre = postre{peso = modificar (peso postre)}
 
 agregarSabor :: String -> Postre -> Postre
 agregarSabor sabor postre = postre{sabores = sabor:sabores postre}
 
 -- Incendio: calienta el postre 1 grado y lo hace perder 5% de su peso.
 incendio :: Hechizo
-incendio = modificarpeso (*0.95) . modificarTemp (+1)
+incendio = modificarPeso (*0.95) . modificarTemp (+1)
 
 --Immobulus: congela el postre, llevando su temperatura a 0.
 inmobulus :: Hechizo
@@ -50,12 +50,12 @@ inmobulus = modificarTemp (const 0)
 
 -- Wingardium Leviosa: levanta el postre en el aire y lo deja caer, lo que agrega a sus sabores el sabor “concentrado”. Además, pierde 10% de su peso.
 wingardiumLeviosa :: Hechizo
-wingardiumLeviosa = agregarSabor "concentrado" . modificarpeso (*0.9)
+wingardiumLeviosa = agregarSabor "concentrado" . modificarPeso (*0.9)
 
 -- Diffindo: Corta el postre, disminuyendo su peso en el porcentaje indicado.
 -- Lo tome como que el numero que le pasa es el porcentaje que quiere bajar
 diffindo :: Number -> Hechizo
-diffindo num = modificarpeso (*(1 - (num/100)))
+diffindo num = modificarPeso (*(1 - (num/100)))
 
 -- Riddikulus: Requiere como información adicional un sabor y lo agrega a los sabores que tiene un postre, pero invertido.
 riddikulus :: String -> Hechizo
@@ -72,7 +72,6 @@ avadaKedavra postre = inmobulus postre{sabores = []}
     y 50 gramos, y les hago el hechizo incendio, quedan listos, pero si les hago el hechizo riddikulus con el sabor
     “nomil” no, porque la tarta sigue congelada.  
 -}
-
 postresListos :: [Postre] -> [Postre]
 postresListos = filter estaListo
 
@@ -86,11 +85,21 @@ cantSabores :: Postre -> Number
 cantSabores = length . sabores
 
 -- D) Dado un conjunto de postres en la mesa, conocer el peso promedio de los postres listos. 
-pesoPromedio :: [Postre] -> Number
-pesoPromedio postres = ((sum. map peso . postresListos) postres) / ((length . postresListos) postres)
+pesoPromedioPostresListos :: [Postre] -> Number
+pesoPromedioPostresListos = (promedio . map peso . postresListos)
+
+promedio :: [Number] -> Number
+promedio lista = sum lista / length lista
 
 -- Ejercicio 2) Magos 
 -- De un mago se conocen sus hechizos aprendidos y la cantidad de horrorcruxes que tiene.
+
+data Mago = UnMago{
+    hechizos :: [Hechizo],
+    cantHorrorCruxes :: Number
+}deriving (Show,Eq)
+
+messi = UnMago [incendio,wingardiumLeviosa] 7
 
 {-
     A) Hacer que un mago asista a la clase de defensa contra las cocinas oscuras y practique con un hechizo sobre un
@@ -98,11 +107,13 @@ pesoPromedio postres = ((sum. map peso . postresListos) postres) / ((length . po
     Cuando un mago practica con un hechizo, lo agrega a sus hechizos aprendidos. 
     Además si el resultado de usar el hechizo en el postre es el mismo que aplicarle “avada kedavra” al postre, entonces suma un horrorcrux.
 -}
+practicarHechizo :: Hechizo -> Postre -> Mago -> Mago
+practicarHechizo hechizo postre = agregarHechizo hechizo . sumarHorrorCruxSiCumple hechizo postre
 
-data Mago = UnMago{
-    hechizos :: [Hechizo],
-    cantHorrorCruxes :: Number
-}deriving (Show,Eq)
+sumarHorrorCruxSiCumple :: Hechizo -> Postre -> Mago -> Mago
+sumarHorrorCruxSiCumple hechizo postre mago
+    | hechizo postre == avadaKedavra postre = sumarHorrorCrux mago
+    | otherwise = mago
 
 agregarHechizo :: Hechizo -> Mago -> Mago
 agregarHechizo hechizo mago = mago{hechizos = hechizo:hechizos mago}
@@ -110,17 +121,9 @@ agregarHechizo hechizo mago = mago{hechizos = hechizo:hechizos mago}
 sumarHorrorCrux :: Mago -> Mago
 sumarHorrorCrux mago = mago{cantHorrorCruxes = cantHorrorCruxes mago + 1}
 
-practicarHechizo :: Postre -> Hechizo -> Mago -> Mago
-practicarHechizo postre hechizo = agregarHechizo hechizo . sumarHorrorCruxSiCumple postre hechizo
-
-sumarHorrorCruxSiCumple :: Postre -> Hechizo -> Mago -> Mago
-sumarHorrorCruxSiCumple postre hechizo mago
-    | hechizo postre == avadaKedavra postre = sumarHorrorCrux mago
-    | otherwise = mago
-
 -- B) Dado un postre y un mago obtener su mejor hechizo, que es aquel de sus hechizos que deja al postre con más cantidad de sabores luego de usarlo.
 mejorHechizo :: Postre -> Mago -> Hechizo
-mejorHechizo postre mago = masCantidaDeSabores postre (hechizos mago)
+mejorHechizo postre = masCantidaDeSabores postre . hechizos
 
 masCantidaDeSabores :: Postre -> [Hechizo] -> Hechizo
 -- Supongo que no existen los magos que no sepan hechizos
@@ -134,24 +137,24 @@ masCantidaDeSabores postre (h1:h2:hs)
 infinitosPostres :: [Postre]
 infinitosPostres = tarta:infinitosPostres
 
+merlin = UnMago infinitosHechizos 5
+
 infinitosHechizos :: [Hechizo]
 infinitosHechizos = incendio:inmobulus:infinitosHechizos
-
-merlin = UnMago infinitosHechizos 5
 
 {-
     B) Suponiendo que hay una mesa con infinitos postres, y pregunto si algún hechizo los deja listos ¿Existe alguna consulta que pueda
     hacer para que me sepa dar una respuesta? Justificar conceptualmente.
-    RTA: En este caso, lazy evaluation no nos ayudaria ya que para saber si todos quedan listos, deberia utilizar un all entonces leeria 
-    la lista entera de  postres (algo imposible si es infinita) para determinar si todos los postres estan listos. Esto no seria posible
-    ya que se quedaria en un bucle infinito.
-    Por otro lado, (aunque seria una trampita) si yo se que la lista de postres repite siempre el mismo postre, como es el caso de la que yo declare,
-    se podria evaluar solo si la cabeza de la fila esta lista. Pero esto solo funcionaria para los casos en los que ya conozco la lista de antemano.
+    RTA: Tomo como que la funcion recibiria una lista de hechizos y una lista de postres. En este caso, lazy evaluation nos ayudaria solo
+    si es falso ya que para saber si todos quedan listos, deberia utilizar un any para la lista de hechizos y un all para la lista de postres
+    entonces tendria que leer la lista entera de postres (si es verdadero, algo imposible si es infinita) para determinar si todos los postres
+    estan listos. Por otro lado, si todos los hechizos devuelven por lo menos un postre que no este listo, entonces devolveria False. Ya que todos
+    los all devolverian False. Y el any tambien daria False
 -}
 
 {-
     C) Suponiendo que un mago tiene infinitos hechizos ¿Existe algún caso en el que se puede encontrar al mejor hechizo? Justificar conceptualmente.
-    RTA: Al hacer mejorHechizo tarta merlin, me devuelve una funcion, que seria un hechizo pero en el momento de aplicarsela a un postre, rompe el programa.
-    Por lo que me parece que nunca encuentra el mejor hechizo, ya que esta hecha con recursividad y el caso base es cuando queda un solo hechizo, situacion
-    que seria imposible si le paso una lista infinita
+    RTA: En este caso, lazy evaluation no nos ayudaria ya que para saber el mejor hechizo debe leer la lista entera hasta que quede uno solo. Por lo que
+    si es infinita, por mas que vayas sacando hechizos, nunca va a quedar uno solo. No hay ningun caso en el que se pueda encontrar el mejor hechizo.
+    Solo se podria saber (como en el punto anterior) si sabemos que la lista repite siempre el mismo hechizo, entonces devolvemos ese hechizo.` 
 -}
